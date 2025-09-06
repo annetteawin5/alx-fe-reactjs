2ebfb40c5044afe197934e4540f40a21e26747d3
Annette Awin <annetteawin5@gmail.com>
2025-09-06 15:21:04 +0100

    Updated commit

---
+++ alx-react-app-props/src/UserDetails.jsx
@@ -0,0 +1,14 @@
+import UserContext from "./components/UserContext";
+import { useContext } from "react";
+
+function UserDetails() {
+const Data = useContext(UserContext)
+  return (
+    <div>
+      <p>Name: {Data.name}</p>
+      <p>Email: {Data.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createContext } from "react";
 
-const UserContext = createContext();
+const UserContext =React.createContext();
 
 export default UserContext;
