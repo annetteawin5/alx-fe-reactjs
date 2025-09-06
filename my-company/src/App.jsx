b3ce006e7e00ca50e7cc15de1918eee011f5f8b4
Annette Awin <annetteawin5@gmail.com>
2025-09-06 23:51:45 +0100

    Initial commit

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -17,7 +17,7 @@ function App () {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/footer" element={<footer />} />
+        
       </Routes>
     </BrowserRouter>
     
