af8cab5674784f8d494d859589e732d6987a176a
Annette Awin <annetteawin5@gmail.com>
2025-09-09 18:39:29 +0100

    Initial commit

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
 
   return (
     <button onClick={() => deleteRecipe(recipeId)}>Delete Recipe</button>
