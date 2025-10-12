951e084f2a48cb97c1936f71b68e82a56842bd2e
Annette Awin <annetteawin5@gmail.com>
2025-10-12 17:41:22 +0100

    Corrected file

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
 
+
+import React from 'react';
 
 const TodoList = ({ todos, onToggleTodo, onDeleteTodo }) => {
   return (
     <ul>
-      {todos.map((todo) => (
+      
+      {(todos || []).map((todo) => (
         <li
           key={todo.id}
           
+          style={{ textDecoration: todo.completed ? 'line-through' : 'none', cursor: 'pointer' }}
           onClick={() => onToggleTodo(todo.id)}
-         
-           
         >
           <span>{todo.text}</span>
-          
           
           <button 
             onClick={(e) => {
               e.stopPropagation(); 
               onDeleteTodo(todo.id);
             }}
-            
           >
             Delete
           </button>
